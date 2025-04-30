845d30d230d367d181c53bcb60fe88700c379e53
smp2kayen <smpduakayen@gmail.com>
2025-05-01 00:32:55 +0700

    Initial commit

--- Si-GERCABfix/pages/_app.js
+++
@@ -1,8 +0,0 @@
-import '../styles/globals.css';
-import '../styles/theme.css';   // custom tampilan
-
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-  
-}
